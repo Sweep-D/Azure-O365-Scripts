ed1b0ab109f74efbeb126ff20e224a980adc08b1
Sweep-D <32283724+Sweep-D@users.noreply.github.com>
2020-03-04 10:47:24 +1100

    Add files via upload

---
+++ get_mfanumber.ps1
@@ -0,0 +1,19 @@
+﻿## Get all users MFA Phone number and stores them in a CSV file
+Connect-MsolService
+$users = Get-MsolUser -All | where {$_.isLicensed -eq $true}
+$upn = $users.userprincipalname
+$csv = "C:\mfa.csv"
+
+try {
+    New-Item $csv -ItemType File -Value ("upn,MFA_Num" + [Environment]::NewLine)
+}
+catch {
+    Add-Content $csv ("upn,MFA_Num")
+}
+
+foreach ($user in $upn) {
+
+    $userdetails = Get-MsolUser -userprincipalname $user
+    $userMFANumber = $userdetails.StrongAuthenticationUserDetails.PhoneNumber
+    Add-Content $csv ($user + "," + $userMFANumber)
+}
